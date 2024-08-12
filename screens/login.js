9d3e7a8085b34ba1ecca5c5421d2bfb28cacf33a
vinne25 <vinicius.silva.leite@hotmail.com>
2024-08-12 20:52:22 -0300

    Update login.js

--- screens/login.js
+++ screens/login.js
@@ -1,10 +1,15 @@
 import React from "react";
-import { View,Text } from "react-native";
+import { View,Text, TouchableOpacity } from "react-native";
+import styles from "./style";
 
 const LOGIN = ({navigation}) =>{
     return(
         <View>
             <Text>Olá </Text>
+        <TouchableOpacity
+        onPress={() => navigation.navigate('pesquisar')}>
+        <Text>Continuar</Text>
+        </TouchableOpacity>
         </View>
     )
 };
