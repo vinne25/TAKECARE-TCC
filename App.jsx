043ffc9e2656d4e1fa41058ad776cef9f9136773
JoyfullRobin <thiagonara1@gmail.com>
2024-08-13 22:28:03 -0300

    Atualizando telas de cadastro

--- App.jsx
+++ App.jsx
@@ -1,4 +1,4 @@
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
+//import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -6,7 +6,8 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Seguranca from './screens/Seguranca';
 import conexoes from './screens/Conexoes';
 import pesquisar from './screens/pesquisar';
-import LOGIN from './screens/login';
+import Login from './screens/login';
+import Finalidade from './screens/finalidade';
 
 const Stack = createStackNavigator();
 
@@ -17,7 +18,8 @@ const App = () =>  {
                 <Stack.Screen name="Seguranca" component={Seguranca} />
                 <Stack.Screen name="Conexões" component={conexoes} />
                 <Stack.Screen name="pesquisar" component={pesquisar} />
-                <Stack.Screen name='login' component={LOGIN} />
+                <Stack.Screen name='login' component={Login} />
+                <Stack.Screen name='finalidade' component={Finalidade} />
             </Stack.Navigator>
         </NavigationContainer>
     );
--- screens/login.js
+++ screens/login.js
@@ -1,16 +1,122 @@
-import React from "react";
-import { View,Text, TouchableOpacity, Button } from "react-native";
-import styles from "./style";
-
-const LOGIN = ({navigation}) =>{
-    return(
-        <View>
-            <Text>Olá oi </Text>
-            <TouchableOpacity>
-                <Text>texto do botão</Text>
+import { View, Text, StyleSheet, Image , TouchableOpacity, Animatable} from "react-native"
+const Login = ({navigation}) =>{
+    return (
+        <View style={styles.container}>
+            <Image
+            source={require('../../TAKECARE-TCC/screens/Imagens/Login.png')}
+            style={{ width: '100%', height: '100%', opacity: 0.8, }}
+            />
+            <Animatable.Image
+            animation="fadeInUp"
+            source={require('../../TAKECARE-TCC/screens/Imagens/logo baba preta.png')}
+            style={{ width: '100%', position:'absolute', }}
+            resizeMode="contain"
+            />
+            <Animatable.View animation="fadeInUp" style={styles.quadrado}>
+            <Text style={styles.login}> Login </Text>
+            <Text style={styles.emailesenha}>E-mail</Text>
+            <TextInput 
+            style={styles.emailinput}
+            placeholder="Digite seu Email"/>
+            <Text style={styles.emailesenha}>Senha</Text>
+            <TextInput 
+            style={styles.senhainput}
+            placeholder="Digite sua Senha"
+            storeData
+            />
+           <Text style={styles.esqueceu}> Ainda não possui um login?</Text>
+           <TouchableOpacity style={styles.cadastro}>
+            <Text style={styles.cadastrese} onPress={ () => navigation.navigate('Signin')}> Cadastre-se </Text>
             </TouchableOpacity>
+            <TouchableOpacity style={styles.botaum}>
+            <Text style={styles.textobotaum}> Entrar </Text>
+            </TouchableOpacity>
+            </Animatable.View>           
         </View>
-    )
-};
+    );
+}
 
-export default LOGIN;
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        alignItems: 'center'
+    },
+
+    quadrado:{
+        position: 'absolute',
+        borderWidth: 1,
+        width: '75%',
+        borderRadius: 10,
+        alignItems: 'center',
+        marginTop: 25,
+        paddingVertical: 80,
+        backgroundColor: 'white',
+        bottom: 150
+    },
+
+    login:{
+        position:'absolute',
+        top: 30,
+        fontSize:25,
+        fontWeight: 'bold',
+    },
+
+    emailesenha:{
+        marginTop: 20,
+        marginBottom:10,
+        fontSize: 15,
+        fontWeight: 'bold',
+        right: 105
+    },
+
+    emailinput:{
+        backgroundColor: "white",
+        borderRadius: 10,
+        width: '90%', 
+        paddingVertical: 5,
+        paddingStart: 8,
+        borderWidth: 1,
+        borderColor: 'black',
+    },
+
+    senhainput:{
+        backgroundColor: "white",
+        borderRadius: 10,
+        width: '90%', 
+        paddingVertical: 5,
+        paddingStart: 8,
+        borderWidth: 1,
+        borderColor: 'black',
+    },
+
+
+    esqueceu:{
+        fontSize: 10,
+        marginTop: 10,
+        right: 60,
+    },
+
+    cadastrese:{
+        fontSize: 10,
+        marginTop: 10,
+        bottom: 0,
+        left: 17,
+        position:'absolute',
+    },
+
+    textobotaum:{
+        fontWeight: 'bold',
+        alignSelf:'center'
+    },
+
+    botaum:{
+        position: 'absolute',
+        width: '30%',
+        paddingVertical: 5,
+        backgroundColor: '#80f4FC',
+        borderRadius: 10,
+        bottom: 20,
+    }
+})
+
+export default Login;
