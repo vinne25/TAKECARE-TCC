a771c47c548274976443527f74ab16fecaefd42c
JoyfullRobin <thiagonara1@gmail.com>
2024-08-20 22:23:36 -0300

    desing novo das telas

--- screens/pesquisar.js
+++ screens/pesquisar.js
@@ -1,44 +1,50 @@
-import { View, TouchableOpacity, Image, Text, StyleSheet} from 'react-native';
+import { View, TouchableOpacity, Image, Text, StyleSheet, ImageBackground } from 'react-native';
 import styles from './style';
 
 const Pesquisar = ({ navigation }) => {
-    return (
-      <View style={styles.container}>
-        <Image
-          style={styles.img}
-          source={require('../assets/logotk.png')}
-          resizeMode="contain"
-        />
-        <Text style={styles.texto}>Pesquisar</Text>
-        <View style={styles.row}>
+  return (
+    <View style={styles.container}>
+      <Image
+        style={styles.img}
+        source={require('../assets/logotk.png')}
+        resizeMode="contain"
+      />
+      <Text style={styles.texto}>Pesquisar</Text>
+      <View style={styles.row}>
         <View
-        style={{
-          width: 15,
-          height: 15,
-          borderRadius: 100,
-          backgroundColor: '#0BBEE5',
-        }}/>
+          style={{
+            width: 15,
+            height: 15,
+            borderRadius: 100,
+            backgroundColor: '#0BBEE5',
+          }} />
         <Text style={styles.texto2}>Use a barra de pesquisa para
-        encontrar trabalho ou babás na sua area</Text>
-        </View>
-        <View style={styles.row2}>
+          encontrar trabalho ou babás na sua área</Text>
+      </View>
+      <View style={styles.row2}>
         <View
-        style={{
-          width: 15,
-          height: 15,
-          borderRadius: 100,
-          backgroundColor: '#0BBEE5',
-        }}/>
+          style={{
+            width: 15,
+            height: 15,
+            borderRadius: 100,
+            backgroundColor: '#0BBEE5',
+          }} />
         <Text style={styles.texto2}>Use os filtros para pesquisar baseados nas
-        suas necessidades</Text>
-        </View>
-        <TouchableOpacity
+          suas necessidades</Text>
+      </View>
+      <ImageBackground
+          source={require('../assets/layout.png')}
+          style={styles.imageBackground}  
+          imageStyle={{ borderRadius: 10 }}  
+        >
+      <TouchableOpacity
         style={styles.button}
         onPress={() => navigation.navigate('Conexões')}>
-        <Text style={styles.buttonText}>Continuar</Text>
-        </TouchableOpacity>
-      </View>
-    );
+          <Text style={styles.buttonText}>Continuar</Text>
+      </TouchableOpacity>
+      </ImageBackground>
+    </View>
+  );
 };
 
 export default Pesquisar;
