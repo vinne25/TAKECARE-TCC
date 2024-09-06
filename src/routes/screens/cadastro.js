d862b962644b40187d8a15dc0ddb7cec3ef44b6c
vinne25 <vinicius.silva.leite@hotmail.com>
2024-09-05 22:58:09 -0300

    Criando a autentificação

--- src/routes/screens/cadastro.js
+++ src/routes/screens/cadastro.js
@@ -1,10 +1,13 @@
 import React from "react";
-import { Text, View, TouchableOpacity } from "react-native";
+import { Text, View, TouchableOpacity, TextInput, StyleSheet } from "react-native";
 
 const Cadastro = ({navigation}) =>{
     return(
-        <View>
+        <View style={styles.conteiner}>
             <Text>Cadastro</Text>
+            <TextInput style={styles.textinput} placeholder="Email:"></TextInput>
+            <TextInput style={styles.textinput} placeholder="Nome:"></TextInput>
+            <TextInput style={styles.textinput} placeholder="Senha:"></TextInput>
             <TouchableOpacity onPress={() => navigation.navigate('LOGIN')}>
                 <Text>Cadastro</Text>
             </TouchableOpacity>
@@ -12,4 +15,24 @@ const Cadastro = ({navigation}) =>{
     )
 }
 
+const styles = StyleSheet.create({
+    conteiner:{
+        flex: 1,
+        alignContent: 'center',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    textinput:{
+        borderStyle: 'solid',
+        borderRadius: 15,
+        borderWidth: 2,
+        width: 200,
+    },
+    text:{
+        position: 'absolute'
+    }
+})
+
 export default Cadastro;
+
+
--- src/routes/screens/login.js
+++ src/routes/screens/login.js
@@ -1,15 +1,37 @@
-import React from "react";
-import { Text, View, TouchableOpacity } from "react-native";
+import React, {useState} from "react";
+import { Text, View, TouchableOpacity, StyleSheet, TextInput } from "react-native";
+import auth from '@react-native-firebase/auth';
 
 const Login = ({navigation}) =>{
     return(
-        <View>
+        <View style={styles.conteiner}>
             <Text>Login</Text>
+            <TextInput style={styles.textinput} placeholder="Email:"></TextInput>
+            <TextInput style={styles.textinput} placeholder="Senha:"></TextInput>
+
             <TouchableOpacity onPress={() => navigation.navigate('TabRoutes')}>
                 <Text>Login</Text>
             </TouchableOpacity>
         </View>
     )
 };
+
+const styles = StyleSheet.create({
+    conteiner:{
+        flex: 1,
+        alignContent: 'center',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    textinput:{
+        borderStyle: 'solid',
+        borderRadius: 15,
+        borderWidth: 2,
+        width: 200,
+    },
+    text:{
+        position: 'absolute'
+    }
+})
 
 export default Login;
