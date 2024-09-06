956e930fa86b32be91eb90836baa3896830e88cd
vinne25 <vinicius.silva.leite@hotmail.com>
2024-09-05 23:57:00 -0300

    Cadastro
    Criando cadastro no firebase

--- src/routes/screens/cadastro.js
+++ src/routes/screens/cadastro.js
@@ -1,38 +1,75 @@
-import React from "react";
-import { Text, View, TouchableOpacity, TextInput, StyleSheet } from "react-native";
+import React, {useState} from 'react';
+import auth from '@react-native-firebase/auth';
+import {
+    Text,
+    View,
+    TouchableOpacity,
+    TextInput,
+    StyleSheet,
+} from 'react-native';
 
-const Cadastro = ({navigation}) =>{
-    return(
+const Cadastro = ({ navigation }) => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    function signUp() {
+        auth()
+          .createUserWithEmailAndPassword(email, password)
+          .then(() => {
+            console.log('User account created & signed in!');
+          })
+          .catch(error => {
+            if (error.code === 'auth/email-already-in-use') {
+              console.log('That email address is already in use!');
+            }
+    
+            if (error.code === 'auth/invalid-email') {
+              console.log('That email address is invalid!');
+            }
+    
+            console.error(error);
+          });
+      }
+    return (
         <View style={styles.conteiner}>
             <Text>Cadastro</Text>
-            <TextInput style={styles.textinput} placeholder="Email:"></TextInput>
-            <TextInput style={styles.textinput} placeholder="Nome:"></TextInput>
-            <TextInput style={styles.textinput} placeholder="Senha:"></TextInput>
+            <TextInput
+                style={styles.textinput}
+                autoCapitalize='none'
+                placeholder='Email'
+                onChangeText={setEmail}></TextInput>
+            <TextInput
+                style={styles.textinput}
+                autoCapitalize='none'
+                placeholder='Senha'                        
+                onChangeText={setPassword}></TextInput>
+                <TouchableOpacity onPress={signUp}>
+                    <Text>Cadastrar</Text>
+                </TouchableOpacity>
             <TouchableOpacity onPress={() => navigation.navigate('LOGIN')}>
-                <Text>Cadastro</Text>
+                <Text>Voltar ao login</Text>
             </TouchableOpacity>
         </View>
-    )
+    );
 }
 
 const styles = StyleSheet.create({
-    conteiner:{
+    conteiner: {
         flex: 1,
         alignContent: 'center',
         justifyContent: 'center',
         alignItems: 'center',
     },
-    textinput:{
+    textinput: {
         borderStyle: 'solid',
         borderRadius: 15,
         borderWidth: 2,
         width: 200,
     },
-    text:{
-        position: 'absolute'
-    }
-})
+    text: {
+        position: 'absolute',
+    },
+});
 
 export default Cadastro;
-
-
--- src/routes/screens/login.js
+++ src/routes/screens/login.js
@@ -3,11 +3,19 @@ import { Text, View, TouchableOpacity, StyleSheet, TextInput } from "react-nativ
 import auth from '@react-native-firebase/auth';
 
 const Login = ({navigation}) =>{
-    return(
+    
+        const [email, setEmail] = useState('');
+        const [password, setPassword] = useState('');
+
+        function signUp(){
+            auth().createUserWithEmailAndPassword(email,password);
+        }
+    
+        return(
         <View style={styles.conteiner}>
             <Text>Login</Text>
-            <TextInput style={styles.textinput} placeholder="Email:"></TextInput>
-            <TextInput style={styles.textinput} placeholder="Senha:"></TextInput>
+            <TextInput style={styles.textinput} placeholder="Email:" value="email" onChangeText={setEmail}></TextInput>
+            <TextInput style={styles.textinput} placeholder="Senha:" value="password" onChangeText={setPassword}></TextInput>
 
             <TouchableOpacity onPress={() => navigation.navigate('TabRoutes')}>
                 <Text>Login</Text>
