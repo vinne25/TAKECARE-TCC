6f111bed693941b0548a6c9ce283fa9a55b5d32f
vinne25 <vinicius.silva.leite@hotmail.com>
2024-09-17 20:15:43 -0300

    react-native-feather

--- src/routes/tab.routes.js
+++ src/routes/tab.routes.js
@@ -1,15 +1,19 @@
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Mapa from './screens/Mapa';
 import Favoritos from './screens/Favoritos';
-import { Feather } from 'react-native-feather';
+import { Map, Heart, User, MapPin} from 'react-native-feather';
 
 const Tab = createBottomTabNavigator();
 
 const TabRoutes = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="MAPA" component={Mapa}/>
-      <Tab.Screen name="FAVORITOS" component={Favoritos}/>
+    <Tab.Navigator screenOptions={{tabBarActiveTintColor: '#0BBEE5', tabBarInactiveTintColor: 'gray'}}>
+      <Tab.Screen name="MAPA" component={Mapa}
+        options={{tabBarIcon: ({ color, size }) => (<MapPin stroke={color} width={size} height={size}/>),}}
+      />
+      <Tab.Screen name="FAVORITOS" component={Favoritos}
+         options={{tabBarIcon: ({ color, size }) => (<Heart stroke={color} width={size} height={size}/>),}}
+      />
     </Tab.Navigator>
   );
 };
